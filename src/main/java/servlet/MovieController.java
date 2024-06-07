080b73eaa36cd51e8055de0687196a1c44638dee
Moontari <shaaa6256@gmail.com>
2024-06-07 16:24:50 +0900

    서블릿 충돌
    충돌 오류수정

--- src/main/java/servlet/MovieController.java
+++ src/main/java/servlet/MovieController.java
@@ -24,35 +24,7 @@ public class MovieController extends HttpServlet {
 			throws ServletException, IOException {
 		String cmd = request.getRequestURI();
 		MovieDAO dao = MovieDAO.getInstance();
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
->>>>>>> a189d8c12e3c0957be0170b883d4fb460c83a59e
-		try {
-
-			if (cmd.equals("/input.movie")) {
-				String title = request.getParameter("");
-<<<<<<< HEAD
-				System.out.println(title);
-				String name = request.getParameter("");
-				System.out.println(name);
-=======
-				String name = request.getParameter("");
->>>>>>> a189d8c12e3c0957be0170b883d4fb460c83a59e
-				response.sendRedirect("index.jsp");
-
-			} else if (cmd.equals("/output.movie")) {
-				List<MovieDTO> list = dao.selectAll();
-				request.setAttribute("list", list);
-				request.getRequestDispatcher("index.jsp").forward(request, response);
-			}
-		} catch (Exception e) {
-<<<<<<< HEAD
-
-		}
-=======
-=======
 		try{
 			
 		
@@ -65,12 +37,11 @@ public class MovieController extends HttpServlet {
 			List<MovieDTO> list = dao.selectAll();
 			request.setAttribute("list", list);
 			request.getRequestDispatcher("outputview.jsp").forward(request, response);
-			
->>>>>>> 84d7db17c5927ab7ca38b47fd439820ca774fda7
-
 		}
 
->>>>>>> a189d8c12e3c0957be0170b883d4fb460c83a59e
+	} catch (Exception e){
+			e.printStackTrace();
+		}
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
